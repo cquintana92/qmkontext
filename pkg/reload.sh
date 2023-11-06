bc5910813ff90db67b1a9b1f497ea0e9d6e6bdc5
Carlos Quintana <carlos@cquintana.dev>
2023-11-06 09:21:58 +0100

    fix: post-install script

--- pkg/reload.sh
+++ pkg/reload.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
+SERVICE_PATH="/etc/systemd/system/qmkontext.service"
+SERVICE_INSTALL_PATH="/usr/share/qmkontext/qmkontext.service"
+
 if ! command -v systemctl &> /dev/null; then
   echo "Could not find systemctl, not reloading"
   exit 0
 fi
 
-if [ "$EUID" -ne 0 ]; then
-  systemctl daemon-reload
+if [ "$EUID" -eq 0 ]; then
+  if [ ! -f "${SERVICE_PATH}" ]; then
+    cp "${SERVICE_INSTALL_PATH}" "${SERVICE_PATH}"
+    systemctl daemon-reload
+  else
+    echo "Service already existed, not replacing"
+  fi
 else
   if command -v sudo &> /dev/null; then
     echo "Could not find sudo, not reloading"
     exit 0
   else
-    sudo systemctl daemon-reload
+    if [ ! -f "${SERVICE_PATH}" ]; then
+        sudo cp "${SERVICE_INSTALL_PATH}" "${SERVICE_PATH}"
+        sudo systemctl daemon-reload
+      else
+        echo "Service already existed, not replacing"
+      fi
   fi
 fi
